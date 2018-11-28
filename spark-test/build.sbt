0d528b36ea181caba46ab0ac0cca5a7bbd40fe63
RespectableRuessel <rocketrolf10@gmail.com>
2018-11-28 14:56:41 +0100

    Plotting support with breeze-viz

--- spark-test/build.sbt
+++ spark-test/build.sbt
@@ -8,3 +8,5 @@ parallelExecution in Test := false
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.0"
 libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.3.0"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5"
+libraryDependencies += "org.scalanlp" %% "breeze" % "0.13.2"
+libraryDependencies += "org.scalanlp" %% "breeze-viz" % "0.13.2"
--- spark-test/src/main/scala/example/StreamingExample.scala
+++ spark-test/src/main/scala/example/StreamingExample.scala
@@ -3,8 +3,11 @@
 package example
 
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
 import org.apache.spark.mllib.linalg
+import breeze.linalg._
+import breeze.plot._
 
 object StreamingExample extends App 
 {
@@ -14,7 +17,9 @@ object StreamingExample extends App
     .set("spark.executor.memory","4g")
     .set("spark.storage.memoryFraction","0.8")
     .set("spark.driver.memory", "2g")
-  val sc= new SparkContext(conf)
+  val spark = SparkSession.builder().appName("SparkTest").config(conf).getOrCreate()
+  val sc = spark.sparkContext
+  
   val data = sc.textFile("../data/adult_short.data")
   
   val a = Processing.degrees
@@ -31,8 +36,38 @@ object StreamingExample extends App
   
   val WSSSE = clusters.computeCost(vectors)
   
-  sc.stop()  
+  val all = vectors.map(f => (f(0), f(1), clusters.predict(f))).collect().toList
 
+  sc.stop()
+  
   println("Within Set sum of Squared Errors = " + WSSSE)
+  
+  val f = Figure()
+  val p = f.subplot(0)
+  
+  for(a <- all.zipWithIndex)
+  {
+    
+    val index = a._2.toDouble
+    val attrA = a._1._1
+    val attrB = a._1._2.toInt
+    val cluster = a._1._3
+    
+    val x = DenseVector.apply(index)
+    val y = DenseVector.apply(attrA)
+    
+    val r = (attrB * 17) % 255
+    val g = (attrB * 5) % 255
+    val b = (attrB * 3) % 255
+    val color = r + "," + g + "," + b
+    
+    val style = Array('.', '+', 'o')(cluster)
+    
+    p += plot(x = x, y = y, style = style, colorcode = color)
+  }
+
+  println(all)
+  
+  f.saveas("clusters.png")
   
 }
